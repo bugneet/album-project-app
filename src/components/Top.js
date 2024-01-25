a85c660120f909f1447879b910b3f4541ec49b8d
kmy <hatsukoi.dev@gmail.com>
2024-01-25 13:37:44 +0900

    a

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import Home from './Home';
+import Total_chart from './Total_chart';
+import Test_chart from './Test_chart';
 
 const Top = () => {
     return (
@@ -12,6 +14,8 @@ const Top = () => {
                     <Link to="/">[전체분석]</Link>
                     <Link to="/">[전시관]</Link>
                     <Link to="/mypage_album">[마이페이지]</Link>
+                    <Link to="/total_chart">[전체분석페이지]</Link>
+                    <Link to="/test_chart">[분석페이지]</Link>
                     <Link to="/">[로그인]</Link>
                     <Link to="/">[회원가입]</Link>
                 </li>
@@ -19,6 +23,8 @@ const Top = () => {
             <hr />
             <Routes>
                 <Route path='/' element={<Home />}></Route>
+                <Route path='/total_chart' element={<Total_chart />} />
+                <Route path='/test_chart' element={<Test_chart />} />
             </Routes>
         </div>
     );
