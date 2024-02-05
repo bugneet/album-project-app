7f4deaa609ddab5614413b08a15e3b254a148c31
hstae0809 <hstae@naver.com>
2024-02-05 13:10:53 +0900

    20240205_1

--- src/components/Analysticpagemenu.js
+++ src/components/Analysticpagemenu.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css' ; 
+
 const Analysticpagemenu = () => {
     return (
         <ul>
@@ -8,6 +9,7 @@ const Analysticpagemenu = () => {
             <li><Link to="/yearly_chart" >연도별 분석</Link></li>
             <li><Link to="/tag_count_yearly_chart">태그별 분석</Link></li>
             <li><Link to="/custom_tags_count_yearly_chart">선호도분석</Link></li>
+            <li><Link to="/UserAnalysis2">사용자 분석</Link></li>
         </ul>
     );
 };
