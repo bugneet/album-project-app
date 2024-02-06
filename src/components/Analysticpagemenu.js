36dbf70ac4e77f576da48777d814b820098ada14
hstae0809 <hstae@naver.com>
2024-02-06 09:15:23 +0900

    20240206_1

--- src/components/Analysticpagemenu.js
+++ src/components/Analysticpagemenu.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../App.css' ; 
+const buttonStyles = {
+    backgroundColor: '#4285F4',
+    border: 'none',
+    color: 'white',
+    borderRadius: '5px',
+    padding: '10px',
+    cursor: 'pointer',
+    textDecoration: 'none',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+};
 
 const Analysticpagemenu = () => {
+    const containerStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',  // 수직 정렬
+        textAlign: 'center',   // 텍스트 정렬
+    };
     return (
-        <ul>
-            <li><Link to="/total_chart">전체분석</Link></li>
-            <li><Link to="/yearly_chart" >연도별 분석</Link></li>
-            <li><Link to="/tag_count_yearly_chart">태그별 분석</Link></li>
-            <li><Link to="/custom_tags_count_yearly_chart">선호도분석</Link></li>
-        </ul>
+        <div style={containerStyle}>
+            <ul style={{ display: 'flex', flexDirection: 'column', listStyle: 'none', width: '200px', padding: 0, lineHeight: '3.5' }}>
+                <li><Link to="/total_chart"style={buttonStyles}>전체분석</Link></li>
+                <li><Link to="/yearly_chart" style={buttonStyles}>연도별 분석</Link></li>
+                <li><Link to="/tag_count_yearly_chart"style={buttonStyles}>태그별 분석</Link></li>
+                <li><Link to="/custom_tags_count_yearly_chart"style={buttonStyles}>선호도분석</Link></li>
+            </ul>
+        </div>
     );
 };
 
