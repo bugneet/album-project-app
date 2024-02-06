fb185eb98065b1a410c901f72c56f5defe24ae40
hstae0809 <hstae@naver.com>
2024-02-06 10:14:47 +0900

    2024020_2

--- src/components/Total_chart.js
+++ src/components/Total_chart.js
@@ -105,7 +105,7 @@ const Total_chart_json = () => {
     return (
         <div id="charDB" style={{ display: 'flex', flexDirection: 'row', marginLeft: '-250px',marginRight :'40px' }}>
             {/* Left side - Menu */}
-            <div id="analysticmenu" style={{ display: 'flex', flexDirection: 'column', border: '1px solid #ddd', borderRadius: '5px', padding: '10px', marginRight: '20px', backgroundColor: '#f2f2f2' ,textAlign: 'center'}}>
+            <div id="analysticmenu" style={{ display: 'flex', flexDirection: 'column', border: '1px solid #ddd', borderRadius: '5px', padding: '10px', marginRight: '20px', backgroundColor: '#f2f2f2',textAlign: 'center'}}>
                 <h2>분석페이지</h2>
                 <Analysticpagemenu></Analysticpagemenu>
             </div>
